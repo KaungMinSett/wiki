98e004c25821abae5f077d598454c2ae3ce4be82
Kaung Min Sat <kaungminsat@Kaungs-MacBook-Pro.local>
2024-04-10 14:57:55 +0630

    entry page finished

---
+++ encyclopedia/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+from . import util
+import markdown2
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def show_entry(request, title):
+    # return error if no title exists
+    if util.get_entry(title) == None:
+        return render(request, "encyclopedia/error.html", {
+            "title": title
+        })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+        'content': util.covert_to_html(title)
+
+
+    })
+
