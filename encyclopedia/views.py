d7f54e5043a8f2debb16ba4fe807e602951f8a43
Kaung Min Sat <kaungminsat@Kaungs-MacBook-Pro.local>
2024-04-11 18:10:17 +0630

    make search funtion case insenstive

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -26,11 +26,11 @@ def search(request):
     entries =[] # list to store the search results
     if request.method == 'POST':
       
-        q = request.POST.get('q') # get the search query
+        q = request.POST.get('q').lower() # get the search query in lower
         if util.get_entry(q) == None:  # if no such title exists then search for the query in the content
             for entry in util.list_entries(): # loop through all the entries
                 content = util.covert_to_html(entry) # convert the entry to html
-                if q in content: # if query is in the content then append the entry to the search result list
+                if q in content.lower(): # if query is in the content in lower case then append the entry to the search result list
                     entries.append(entry)
             return render(request, "encyclopedia/search_result.html", {"entries": entries}) # pass search result list to template
         else:
